bfc7a87224e74693ad627b4ffb4bb6b3cdfe3bc6
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-09-06 20:08:16 +0400

    fix(board): fixed cursor

--- src/Chess/components/Board/Board.jsx
+++ src/Chess/components/Board/Board.jsx
@@ -482,8 +482,8 @@ export default class Board extends React.Component {
       for (let j = 0; j < 8; j++) {
         const copySquares = this.state.squares.slice();
         let squareColor = calcSquareColor(i, j, copySquares);
-        let squareCursor = "pointer";
-        if (copySquares[i * 8 + j].player !== "w") squareCursor = "default";
+        let squareCursor
+        if (copySquares[i * 8 + j].player === this.state.turn) squareCursor = "pointer";
 
         if (this.state.mated) squareCursor = "default";
         if (this.state.historyNum - 1 !== this.state.trueNum)
