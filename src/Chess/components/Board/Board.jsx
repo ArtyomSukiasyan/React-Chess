acf47274557702aadcd91fb08b824f3334dbcf7d
ArtyomSukiasyan <artiom.suqiasyan1401@gmail.com>
2021-11-11 19:35:21 +0400

    refactor: to segregate blockerExist helper function

--- src/Chess/components/Board/Board.jsx
+++ src/Chess/components/Board/Board.jsx
@@ -30,6 +30,7 @@ import {
 } from "../../constants/asciis";
 import { next, back, nextAtw, backAtw } from "../../constants/histories";
 import styles from "../../Game.module.css";
+import blockersExist from "../../helpers/blockerExist";
 
 export default class Board extends React.Component {
   constructor() {
@@ -242,42 +243,6 @@ export default class Board extends React.Component {
     return copySquares;
   }
 
-  blockersExist(start, end, squares) {
-    let startRow = 8 - Math.floor(start / 8);
-    let startCol = (start % 8) + 1;
-    let endRow = 8 - Math.floor(end / 8);
-    let endCol = (end % 8) + 1;
-    let rowDiff = endRow - startRow;
-    let colDiff = endCol - startCol;
-    let rowCtr = 0;
-    let colCtr = 0;
-    const copySquares = squares.slice();
-
-    while (colCtr !== colDiff || rowCtr !== rowDiff) {
-      let position = 64 - startRow * 8 + -8 * rowCtr + (startCol - 1 + colCtr);
-      if (
-        copySquares[position].ascii != null &&
-        copySquares[position] !== copySquares[start]
-      )
-        return true;
-      if (colCtr !== colDiff) {
-        if (colDiff > 0) {
-          ++colCtr;
-        } else {
-          --colCtr;
-        }
-      }
-      if (rowCtr !== rowDiff) {
-        if (rowDiff > 0) {
-          ++rowCtr;
-        } else {
-          --rowCtr;
-        }
-      }
-    }
-    return false;
-  }
-
   goodPawn(start, end, squares, passantPos) {
     let passant = passantPos === undefined ? this.state.passantPos : passantPos;
     let startRow = 8 - Math.floor(start / 8);
@@ -335,7 +300,7 @@ export default class Board extends React.Component {
       copySquares[start].ascii === blackPawn ||
       copySquares[start].ascii === whiteKing ||
       copySquares[start].ascii === blackKing;
-    let invalid = bqrpk && this.blockersExist(start, end, copySquares);
+    let invalid = bqrpk && blockersExist(start, end, copySquares);
     if (invalid) return invalid;
     let pawn =
       copySquares[start].ascii === whitePawn ||
